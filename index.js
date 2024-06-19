2aa0434a3f1a7e0e429363ed3acaae233fd4f897
Adarsh K <adarshbhatk@gmail.com>
2024-06-19 16:41:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+import 'dotenv/config';
+import { neon } from '@neondatabase/serverless';
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const db = neon(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
+
+async function getPgVersion() {
+  const result = await db`SELECT version()`;
+  console.log(result[0]);
+}
+
+getPgVersion();
+
+
+let quiz = [];
+let totalCorrect = 0;
+let currentQuestion = {};
+
+async function queryTable() {
+  try {
+    const result = await db`SELECT * FROM capitals`;
+    quiz = result;
+  } catch (error) {
+    console.error('Error querying the table:', error);
+  }
+}
+
+queryTable();
+
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
