dae2ec6122a6cc9a5a13fcfb3030f0ed296ecea7
Adarsh K <84382482+adarshbhatk@users.noreply.github.com>
2024-06-19 17:20:28 +0530

    Update port value to 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
